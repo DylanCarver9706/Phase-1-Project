cabd5490605534f33d8f5ebc1d07daeb918453ed
Jackson Coleman <wcoleman@colgate.edu>
2022-08-01 12:12:24 -0500

    Added dropdown feature

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ const apiLink = 'https://api.coingecko.com/api/v3/exchange_rates';
 document.addEventListener('DOMContentLoaded',() => {
     fetch(apiLink)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => addCurrenciesToDropdown(data.rates))
 })
 
+// add a dropdown
+const dropdown = document.createElement('select');
+const body = document.querySelector('body');
+
+function addCurrenciesToDropdown(currencies){
+    let currencyKeys = Object.keys(currencies);
+    currencyKeys.forEach(key => {
+        let currency = currencies[key];
+        const option = document.createElement("option");
+        option.value = currency.unit;
+        option.text = currency.name;
+        dropdown.append(option);
+    })
+}
+
+body.append(dropdown);
